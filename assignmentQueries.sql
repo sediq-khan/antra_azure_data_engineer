8805f38c6a9241a1275f9ff210398fc01697f391
Sediq Khan <pro.sediq@gmail.com>
2022-04-29 13:14:21 -0500

    Answer 2

--- assignmentQueries.sql
+++ assignmentQueries.sql
@@ -8,3 +8,16 @@ FROM Application.People AS P
 LEFT JOIN Sales.Customers AS C
 ON (P.PersonID = C.PrimaryContactPersonID) OR (P.PersonID = C.AlternateContactPersonID)
 WHERE FullName NOT LIKE 'Data Conversion%';
+
+--Question 2
+SELECT P.PersonID, P.PhoneNumber, C.PrimaryContactPersonID, C.PhoneNumber, C.CustomerName FROM Sales.Customers AS C
+LEFT JOIN Application.People AS P
+ON C.PrimaryContactPersonID = P.PersonID
+AND P.PhoneNumber = C.PhoneNumber
+
+
+
+
+
+
+
